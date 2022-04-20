a7940c43f0406ef07043e40472ecfb67f3810cba
thiagobrunoms <thiagobrunoms@gmail.com>
2022-04-19 21:15:24 -0300

    :sparkles: SingIn SignUp main page
    Creates the first signin signup page, where a user can choose if he/s goes to the signup page or to the singin page

---
+++ lib/signin_signup/presentation/pages/get_started/signin_signup_get_started_page.dart
@@ -0,0 +1,66 @@
+import 'package:curso_ifal_flutter/signin_signup/presentation/widets/default_button_widget.dart';
+import 'package:flutter/material.dart';
+
+class SignInSignUpGetStartedPage extends StatelessWidget {
+  const SignInSignUpGetStartedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          height: height,
+          width: width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Image.asset(
+                "assets/images/fluorine-app-logo.png",
+                scale: 2,
+              ),
+              Image.asset(
+                "assets/images/onboarding - dentists-front.png",
+                scale: 2,
+              ),
+              Column(
+                children: [
+                  const Text("Escolha uma das opções grátis"),
+                  SizedBox(height: 15),
+                  SizedBox(
+                    width: width * 0.95,
+                    height: height * 0.07,
+                    child: DefaultButton(
+                      color: Theme.of(context).primaryColor,
+                      widget: const Text("Criar Conta",
+                          style: TextStyle(color: Colors.white)),
+                      callback: () {
+                        print("Ir para tela de criar conta");
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  SizedBox(
+                    width: width * 0.95,
+                    height: height * 0.07,
+                    child: DefaultButton(
+                      color: Colors.white,
+                      widget: Text("Criar Conta",
+                          style:
+                              TextStyle(color: Theme.of(context).primaryColor)),
+                      callback: () {
+                        print("Ir para tela de login");
+                      },
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
