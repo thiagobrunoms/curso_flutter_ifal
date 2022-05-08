c4c02f1e120d58e6c4ba182612a0635f0554cfa7
Thiago de Sales <thiagobrunoms@gmail.com>
2022-05-08 17:55:32 -0300

    Fix how Dio is injected - from instance to singleton

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -6,10 +6,6 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'infra_module.dart';
 
 class AppModule extends Module {
-  @override
-  List<Module> get imports => [
-        InfraModule(),
-      ];
 
   @override
   List<ModularRoute> get routes => [
--- lib/infra_module.dart
+++ lib/infra_module.dart
@@ -4,8 +4,8 @@ import 'package:flutter_modular/flutter_modular.dart';
 class InfraModule extends Module {
   @override
   List<Bind> get binds => [
-        Bind.instance(
-            (i) => Dio(BaseOptions(baseUrl: 'http://192.168.0.101:3000')),
+        Bind.singleton(
+            (i) => Dio(BaseOptions(baseUrl: 'http://10.2.2.0:3000')),
             export: true),
       ];
 }
